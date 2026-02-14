690fb32add5cead12526d15878b6447426990bec
Shahnawaz-Bhutto <hajibooshanu777@gmail.com>
2026-02-14 18:29:32 +0500

    adding svg

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
   ]
 
   return (
-    <header className="bg-blue-950 text-white sticky top-0 z-50 shadow-lg">
+    <header className="bg-blue-950 text-white  top-0 z-50 shadow-lg">
       <div className="container mx-auto flex items-center justify-between px-6 lg:px-16 py-4">
 
         <h1 className="text-blue-500 text-2xl sm:text-3xl font-bold">
